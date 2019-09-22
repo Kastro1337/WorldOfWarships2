62ed6a4167abafc52281076663bb61a5354ed2d9
Angelo Castro <50339824+Kastro1337@users.noreply.github.com>
2019-09-21 23:21:41 -0300

    V1.2
    Func:
       -pos_barc
       -print_tab
       -missil
    
    main:
       *apenas testes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,29 @@
+# -*- coding: utf-8 -*-
 import func
-#print(func.cria_matrix())
+__author__ = "Kastro"
+# "That ain't working,
+#  That's the way you do it"
 
-tab1 = cria_matrix()
-tab2 = cria_matrix()
+tab1 = func.cria_matrix(10,10)
+tab2 = func.cria_matrix(10,10)
+
+# Note: tab1[y][x]
+tab1 = func.pos_barco(tab1,0,0,'1','R')
+#tab1[0][0] = 'X'
+
+func.print_tab(tab1)
+tab1 = func.missil(tab1,0,0)
+print("teste")
+func.print_tab(tab1)
+
+#TODO: HIT or MISS aviso
+#TODO: lógica de jogo aplicada
+
+'''
+backuptab1 = tab1
+# Não funciona pq listas tem vida própria
+if backuptab1 == tab1:
+    print("miss")
+else:
+    print('hit')
+'''
