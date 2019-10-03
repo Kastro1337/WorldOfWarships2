52be61265c9bc9e1e2b2ca5abc007b68dac9c817
Angelo Castro <50339824+Kastro1337@users.noreply.github.com>
2019-10-03 08:41:53 -0300

    v1.4.2
    Tem lista e tals ta top

--- src/main.py
+++ src/main.py
@@ -8,19 +8,22 @@ tab1 = func.cria_matrix(10,10)
 tab2 = func.cria_matrix(10,10)
 tab3 = func.cria_matrix(10,10)
 
+tnavios = {"barco de patrulha":2,"submarino":3,"destroyer":3,"encouraçado":4,"porta-avião":5}
+navios = ["barco de patrulha","submarino","destroyer","encouraçado","porta-avião"]
+
 
 for i in range(5):
     print("jogador 1:")
     func.print_tab(tab1)
-    print(i)
-    func.pos_barco(tab1,x = int(input("Digite a Coluna")),y = int(input("Digite a Fileira")), tipo = str(i+1),OneDirection = input("Digite a direção [R/D]"))
+    print(navios[i])
+    func.pos_barco(tab1,x = int(input("Digite a Coluna")),y = int(input("Digite a Fileira")), tipo = tnavios[navios[i]],OneDirection = input("Digite a direção [R/D]"))
 
 for i in range(5):
 
     print("jogador 2:")
     func.print_tab(tab2)
     print(i)
-    func.pos_barco(tab2,x = int(input("Digite a Coluna")),y = int(input("Digite a Fileira")), tipo = str(i+1),OneDirection = input("Digite a direção [R/D]"))
+    func.pos_barco(tab2,x = int(input("Digite a Coluna")),y = int(input("Digite a Fileira")), tipo = tnavios[navios[i]],OneDirection = input("Digite a direção [R/D]"))
 
 
 while tab1 != tab3 or tab2 != tab3:
@@ -30,6 +33,7 @@ while tab1 != tab3 or tab2 != tab3:
     print('\n')
     print("jogador 2:")
     print(func.print_tab(tab2))
+    
 
 # TODO: aplicar lógica de jogo
 # TODO: printar Acertou / errou
