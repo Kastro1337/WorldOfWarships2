e3414776d3b57ae018cc6532bd6a8a3833dde7a1
Angelo Castro <50339824+Kastro1337@users.noreply.github.com>
2019-10-17 08:39:39 -0300

    V1.5
    falta fazer a verificação de x, y e direção funfar 200%

--- src/func.py
+++ src/func.py
@@ -1,76 +1,66 @@
 # -*- coding: utf-8 -*-
 __author__ = "Kastro & Rebelatineo"
 
+'''funções para batalha naval.'''
 
-'''def cria_matrix(vlinha = 40, vcoluna = 40):
-    matriz = []
-    linha = []
-    for i in range(vcoluna):
-        linha.append("0")
-    for i in range(vlinha):
-        matriz.append(linha)
-    return matriz
-
-'''
 def cria_matrix(vlinha, vcoluna):
     # Cria um tabuleiro mto do bacana
     tabuleiro = []
-    for i in range(vlinha):
-        tabuleiro.append(['0']*vcoluna)
+    for i in range(vlinha):             # cria linhas
+        tabuleiro.append(['0']*vcoluna) # cria elementos dentro das linhas, que servem como coluna as well
     return tabuleiro
 
 def print_tab(tabuleiro):
     # Printa esse tabuleiro de maneira razoável
-    for i in tabuleiro:
+    for i in tabuleiro:     # pega um elemento do tabuleiro individualmente.
         print(" ".join(i))
 
 def missil(tab,x,y):
     # manda um teleguiado, very OP
-    if tab[int(y)][int(x)] == '0':
-        print("Errou")
+    if tab[int(y)][int(x)] == '0': # se a posição no tabuleiro já for igual a 0 (nulo/água) - (x,y) invertido
+        print("Errou")             # mostra erro
         return tab
     else:
-        print("Acertou!!!!")
-        tab[int(y)][int(x)] = '0'
+        print("Acertou!!!!")       # senão mostra o acerto
+        tab[int(y)][int(x)] = '0'  # e substitui a posição
         return  tab
 
 
-def pos_barco(tab, x, y, tipo, OneDirection='R'):
-    # zygmunt barcman
-    if  OneDirection.upper() == "R":
+def pos_barco(tab, x, y, tipo, OneDirection='D'):
+    # zygmunt barcman. posiciona o barco no tabuleiro
+    if  OneDirection.upper() == "D": # posicionado para a direita
+        for i in range(int(tipo)):   # O tipo é um número em formato de string
+            tab[y][x+i] = tipo       # as casas posicionadas serão o alcance do tipo para a direita
+    elif OneDirection.upper() == "B":
         for i in range(int(tipo)):
-            tab[y][x+i] = tipo
-    elif OneDirection.upper() == "D":
-        for i in range(int(tipo)):
-            tab[y+i][x] = tipo 
-    else:
-        raise "Erro, deve ser direção"
+            tab[y+i][x] = tipo       # as casas posicionadas serão o alcance do tipo para baixo
+    # else:
+    #     raise "Erro, deve ser direção"
     return tab
 
 
 
 def verifica_se_a_posicao_que_o_usuario_escolheu_eh_valida(tipo):
-    
+
     while True:
-        x = int(input("Digite a Coluna (x)"))
+        x = int(input("Digite a Coluna (x)"))     # pede X e Y e direção dentro da função
         y = int(input("Digite a Fileira (y)"))
 
-        OneDirection = str(input("Digite a direção [R/D]")).upper()
-        
-        
-        
-        
-        
-     #   print('aquii')
-        
-        if OneDirection == 'R' or  OneDirection == 'D'  :
-         #   print('Caiu aquiii')
-            if y + int(tipo) < 10 or  x + int(tipo) < 10 :
-                
-          #      print('Caiu aqui')
-                break
-        print('Valor invalido, Digite novamente \n \n \n ')
-                
+        OneDirection = str(input("Digite a direção [D/B]")).upper()
 
-    return (x,y,OneDirection)
-    
+
+
+
+
+        if OneDirection == 'D' or  OneDirection == 'B'  : 	#
+            if y in range(1,10) or x in range(1,10):		# TODO: trocar esses 10 por uma váriavel que é o range do tabuleiro
+                print('Não é um valor valido')			# por que y e x in range(1,10) são inválidos??????
+                break
+
+
+            if y + int(tipo) < 10 or  x + int(tipo) < 10 :	# se a posição mais o tipo < 10, então é válido e sai do laço while
+                break
+        print('Valor invalido, Digite novamente \n \n \n ') # retorna ao começo do laço, pois não verificou-se posições válidas.
+
+
+    return (int(x),int(y),OneDirection) # retorna o x , y e a direção válidos
--- src/main.py
+++ src/main.py
@@ -3,10 +3,9 @@ import func
 __author__ = "Rebellatto , Kastro"
 # "That ain't working,
 #  That's the way you do it"
-#Minha versão
 
 tamanho_tab = 10
- 
+
 tab1 = func.cria_matrix(tamanho_tab,tamanho_tab)
 tab2 = func.cria_matrix(tamanho_tab,tamanho_tab)
 
@@ -20,10 +19,10 @@ for i in range(5):
     print("jogador 1:")
     func.print_tab(tab1)
     print(navios[i])
-    tipo =  tnavios[navios[i]] 
-    
+    tipo =  tnavios[navios[i]]
+
     verific = func.verifica_se_a_posicao_que_o_usuario_escolheu_eh_valida(tipo)
-    
+
     func.pos_barco(tab1,verific[0],verific[1], tipo,verific[2])
 
 func.print_tab(tab1)
@@ -33,9 +32,9 @@ for i in range(5):
     print("jogador 2:")
     func.print_tab(tab2)
     print(navios[i])
-    tipo =  tnavios[navios[i]] 
+    tipo =  tnavios[navios[i]]
     verific = func.verifica_se_a_posicao_que_o_usuario_escolheu_eh_valida(tipo)
-    
+
     func.pos_barco(tab2,verific[0],verific[1], tipo,verific[2])
 
 func.print_tab(tab2)
@@ -50,7 +49,8 @@ while tab1 != tab3 or tab2 != tab3:
     func.print_tab(tab2)
     tab1 = func.missil(tab1,x = str(input("Digite a coluna")), y = str(input("Digite a Fileira:")))
     print('\n')
-    
-# TODO: printar Acertou / errou
 
-
+if tab1 == tab3:
+    print("JOGADOR 2 GANHOU!!!")
+else:
+    print("JOGADOR 1 GANHOU!!!"")
