97a5250adf59110e688abaf6fd9be0b00620025e
Guilherme <51514376+guilherme963@users.noreply.github.com>
2019-11-04 16:58:37 -0300

    sei la

--- src/func.py
+++ src/func.py
@@ -37,70 +37,69 @@ def pos_barco(tab, x, y, tipo, OneDirection='D'):
 
                 for i in range(  int(tipo) ):
                     if tab[y][x+i] == tipo:   tab[y][x+i] = '0'
-  
-                    
+
+
                 print('Opha opa opa você não pode colocar seu barco aqui amigão')
                 print('Pedeu um barco OTARIO')
                 break
-                
+
             else:   tab[y][x+i] = tipo       # as casas posicionadas serão o alcance do tipo para a d
-        
-            
 
 
 
-                
+
+
+
     elif OneDirection.upper() == "B":
         for i in range(int(tipo)):
             if tab[y + i][x] != '0':
 
                 for i in range(  int(tipo) ):
                     if tab[y+i][x] == tipo:   tab[y+i][x] = '0'
-  
 
-                
+
+
 
                 print('Opha opa opa você não pode colocar seu barco aqui amigão')
                 print('Pedeu um barco OTARIO')
 
                 break
             else: tab[y+i][x] = tipo       # as casas posicionadas serão o alcance do tipo para baixo
-    
-    
+
+
     return tab
 
 
 
 
-    
+
 def verifica_se_a_posicao_que_o_usuario_escolheu_eh_valida(tipo):
-    
+
     while True:
-        
+
         x , y = int(input("Digite a Coluna (x)")) , int(input("Digite a Fileira (y)"))   # pede X e Y e direção dentro da função
 
         OneDirection = str(input("Digite a direção [D/B]")).upper()
-        
-    
+
+
 
 
 
 
         if OneDirection == 'D' or  OneDirection == 'B'  :
 
-            
+
             if not y in range(0,10) or  x not in range(0,10):
                 print('Não é um valor valido') 
             else:
-                
-                
+
+
                 if y + int(tipo) < 10 or  x + int(tipo) < 10 : return (int(x),int(y),OneDirection) 	
                 else: print('Nossos barcos não chegam lá comandante')
-                    
-                    
 
-            
+
+
+
         else: print(OneDirection, 'Não e valido como posição') # retorna ao começo do laço, pois não verificou-se posições válidas.
-
 
 
